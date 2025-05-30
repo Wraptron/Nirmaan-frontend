d7bc9d25e0c53fa8c19127e583baee074fe8b855
krish214 <krish21498@gmail.com>
2025-05-30 16:59:56 +0530

    build_v1

--- src/pages/Mentors/EditMentorPage.js
+++ src/pages/Mentors/EditMentorPage.js
@@ -206,7 +206,7 @@ const EditMentorForm = ({ initialData, onClose, onSubmit }) => {
               type="submit"
               className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
             >
-              Save Changes
+              Upadte
             </button>
           </div>
         </form>
