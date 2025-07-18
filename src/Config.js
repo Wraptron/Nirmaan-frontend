3eaac48a7f387bf1bd558c61428f036383f769d2
krish214 <krish21498@gmail.com>
2025-07-18 11:44:42 +0530

    ip changed

--- src/Config.js
+++ src/Config.js
@@ -4,7 +4,7 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 const API_URLS = {
   DEVELOPMENT: "http://localhost:3003/api/v1/",
-  PRODUCTION: "https://3.110.173.141/api/v1/", // Corrected IP address
+  PRODUCTION: "https://3.109.144.218/api/v1/", // Corrected IP address
 };
 
 const APP_URL = isDevelopment ? API_URLS.DEVELOPMENT : API_URLS.PRODUCTION;
