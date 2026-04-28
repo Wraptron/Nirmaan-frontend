89ba0f8e60c26d44eb0a7331bea229cf3a7d710d
Charukash <charucharukash45@gmail.com>
2026-04-28 12:20:39 +0530

    Refactor API and Socket URL configurations to support environment variables.

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,15 @@
 import {io} from 'socket.io-client';
 
-const URL = 'http://localhost:5000';
+const isDevelopment = process.env.NODE_ENV === "development";
+const DEFAULT_SOCKET_URL = isDevelopment
+  ? "http://localhost:5000"
+  : "http://nirmaan-api-newalb-659762370.ap-south-1.elb.amazonaws.com";
 
-export const socket = io(URL);
+const SOCKET_URL = (
+  process.env.REACT_APP_SOCKET_URL ||
+  DEFAULT_SOCKET_URL
+).replace(/\/+$/, "");
+
+export const socket = io(SOCKET_URL, {
+  transports: ["websocket", "polling"],
+});
